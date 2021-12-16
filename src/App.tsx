f50a3a54247f29856c46c50fa24ee803f73a201c
QuantumNg <wujs16@gmail.com>
2021-12-16 20:19:10 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState, createContext } from 'react'
-import { Routes, Route, Link } from "react-router-dom";
+import { useState, useContext, createContext } from 'react'
+import { Routes, Route, Link, useLocation, Navigate, useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { RootState } from '@store';
 import './test.scss'
@@ -28,6 +28,25 @@ interface AuthContextType {
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
   }
 
+  function useAuth() {
+    return useContext(AuthContext);
+  }
+
+  function RequireAuth({ children }: { children: JSX.Element }) {
+    let auth = useAuth();
+    let location = useLocation();
+  
+    if (!auth.user) {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      return <Navigate to="/login" state={{ from: location }} />;
+    }
+  
+    return children;
+  }
+
 function App() {
     const userInfo = useSelector((state: RootState) => state.user.userInfo);
 
@@ -53,9 +72,7 @@ function App() {
                         <RequireAuth>
                             <ProtectedPage />
                         </RequireAuth>
-                    }
-                />
-        </Route>
+                    } />
             </Routes>
         </div>
         </AuthProvider>
@@ -92,5 +109,48 @@ function About() {
         </>
     );
 }
+
+function LoginPage() {
+    let navigate = useNavigate();
+    let location = useLocation();
+    let auth = useAuth();
+  
+    let from = location.state?.from?.pathname || "/";
+  
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+  
+      let formData = new FormData(event.currentTarget);
+      let username = formData.get("username") as string;
+  
+      auth.signin(username, () => {
+        // Send them back to the page they tried to visit when they were
+        // redirected to the login page. Use { replace: true } so we don't create
+        // another entry in the history stack for the login page.  This means that
+        // when they get to the protected page and click the back button, they
+        // won't end up back on the login page, which is also really nice for the
+        // user experience.
+        navigate(from, { replace: true });
+      });
+    }
+  
+    return (
+      <div>
+        <p>You must log in to view the page at {from}</p>
+  
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username: <input name="username" type="text" />
+          </label>{" "}
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+
+
+  function ProtectedPage() {
+    return <h3>Protected</h3>;
+  }
 
 export default App
